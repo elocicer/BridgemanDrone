6f684919d82981c2925b2a365a649a38030a7a81
elocicer <ejl48@duke.edu>
2024-04-22 14:38:39 -0400

    implemented old ESC arming procedure

--- ESC_take2.py
+++ ESC_take2.py
@@ -14,41 +14,29 @@ def init():
     print("Motors connected and callibrated!")
     return pins, mypi
 
-def connectMotorsPigpio(pins, relay_pin):
+def connectMotorsPigpio(pins):
+    # Setup and connect motors.
     print("Connecting motors...")
-    '''
-    This function automatically performs the ESC calibration procedure. 
-    First, power is disconnected from the motors, and the ESC PWM signals are set to high.
-    Then, power is connected, and the ESC PWM signals are set to low.
-    At this point, the ESCs should beep 4 times (depending on the model) 
-    to indicate the motors are calibrated and armed.
-    The operator provides verification via keystroke to complete the procedure.
-    '''
-
-    # Min and max pulsewidth values
+    # Pins are as set on BOARD not by GPIO ordering.
+    
+    mypi = pigpio.pi()
+    for pin in pins:
+        mypi.set_servo_pulsewidth(pin, 0)
+    
     maxval = 1900
     minval = 1100
-
-    # Create rpi object
-    mypi = pigpio.pi()
     
-    # Ensure relay is disconnected and ESCs are set to high
-    mypi.write(relay_pin, 0)
-    time.sleep(2)
-    mypi.write(relay_pin, 1)
+    input("Check that battery is disconnected from the base, then press enter.")
     for pin in pins:
-        mypi.set_servo_pulsewidth(pin, minval)
-    time.sleep(2)
-    
-    # Connect relay
+        mypi.set_servo_pulsewidth(pin, minval) # yellow ESCs can arm from minval
+        
+    input("Connect the battery. Press enter when done.")
     for pin in pins:
         mypi.set_servo_pulsewidth(pin, minval)
     print("4 beeps indicates drone is armed. Otherwise, needs callibration.")
     print("End arming sequence.\n\n")
     input("Press any key to continue.")
-
-    #
-    print("Starting program. To stop drone, press Ctrl+C.")
+    print("Starting program. To kill drone, kill the program using Ctrl+C.")
     time.sleep(3)
     return mypi
 
