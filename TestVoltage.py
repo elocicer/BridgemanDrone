25d7cb43e43f20db2d965dac83452b4653d36931
alexpenne1 <alexander.penne@duke.edu>
2024-07-10 13:28:43 -0400

    minor change to test script

--- TestVoltage.py
+++ TestVoltage.py
@@ -23,10 +23,11 @@ import busio
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 
-R1 = 13.3 #kOhm
+R1 = 18.25 #kOhm
 R2 = 10 #kOhm
 i2c = busio.I2C(board.SCL,board.SDA)
 ads = ADS.ADS1115(i2c)
 channel = AnalogIn(ads,ADS.P0)
+print(f"Split Voltage: {channel.voltage:2.2f}V")
 v_battery = channel.voltage * ((R1+R2)/R2)
 print(f"Battery Voltage: {v_battery:2.2f}V")
